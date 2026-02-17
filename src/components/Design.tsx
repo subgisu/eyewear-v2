7ea294f2a1b840c1466f35b5f5bdee1b132c92a8
subgisu <annjin4@gmail.com>
2026-02-17 18:33:19 +0900

    Update Design.tsx

--- src/components/Design.tsx
+++ src/components/Design.tsx
@@ -20,8 +20,8 @@ const Design = () => {
           <div key={idx} className="flex flex-col">
             <div className="aspect-[3/4] overflow-hidden bg-sand/20">
               <img
-                src={`/eyewear-v2/images/point1.png=${idx + 10}`}
-                alt={`디자인 디테일 ${idx + 1}`}
+                src={`/eyewear-v2/images/point${idx + 1}.png`}
+alt={`디자인 디테일 ${idx + 1}`}
                 className="w-full h-full object-cover hover:scale-105 transition-transform duration-700"
               />
             </div>
